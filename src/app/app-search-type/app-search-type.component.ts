a532e68a12cd5e505010802f264af2d685c7793c
Even <zhangcp1220@thundersoft.com>
2019-05-19 16:33:33 +0800

    fix

--- src/app/app-search-type/app-search-type.component.ts
+++ src/app/app-search-type/app-search-type.component.ts
@@ -283,17 +283,17 @@ export class AppSearchTypeComponent {
     this.status.resultList.searchResultIndex = 0;
     this.searchResult.caseList.param.bookName = data.bookName;
     this.searchResult.bookAuthorList.param.bookName = data.bookName;
-    // if (this.status.diseases || !this.status.topInputSearch.searchstr) {
+    if (this.status.diseases) {
       this.searchResult.bookAuthorList.initList();
       this.searchResult.caseList.initList();
-    // } else {
-    //   this.getCategoryList().subscribe(ids => {
-    //     this.searchResult.bookAuthorList.param.bookCataId = ids;
-    //     this.searchResult.caseList.param.bookCataId = ids;
-    //     this.searchResult.bookAuthorList.initList();
-    //     this.searchResult.caseList.initList();
-    //   });
-    // }
+    } else {
+      this.getCategoryList().subscribe(ids => {
+        this.searchResult.bookAuthorList.param.bookCataId = ids;
+        this.searchResult.caseList.param.bookCataId = ids;
+        this.searchResult.bookAuthorList.initList();
+        this.searchResult.caseList.initList();
+      });
+    }
   }
 
   // 点击医家 查询
@@ -322,17 +322,17 @@ export class AppSearchTypeComponent {
     this.status.resultList.searchResultIndex = 0;
     this.searchResult.caseList.param.bookAuthor = data.bookAuthor;
     this.searchResult.bookNameList.param.bookAuthor = data.bookAuthor;
-    // if (this.status.diseases || !this.status.topInputSearch.searchstr) {
+    if (this.status.diseases) {
       this.searchResult.bookNameList.initList();
       this.searchResult.caseList.initList();
-    // } else {
-    //   this.getCategoryList().subscribe(ids => {
-    //     this.searchResult.bookNameList.param.bookCataId = ids;
-    //     this.searchResult.caseList.param.bookCataId = ids;
-    //     this.searchResult.bookNameList.initList();
-    //     this.searchResult.caseList.initList();
-    //   });
-    // }
+    } else {
+      this.getCategoryList().subscribe(ids => {
+        this.searchResult.bookNameList.param.bookCataId = ids;
+        this.searchResult.caseList.param.bookCataId = ids;
+        this.searchResult.bookNameList.initList();
+        this.searchResult.caseList.initList();
+      });
+    }
   }
 
   // 点击 分类查询模式
